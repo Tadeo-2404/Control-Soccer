795d9bfb3be629e2bf8220445b87ac9ca9dfd3ee
George5376 <luismarguz2003@gmail.com>
2023-12-05 22:51:15 -0600

    AgregarCobro concectado

--- client/src/components/AgregarCobro.jsx
+++ client/src/components/AgregarCobro.jsx
@@ -1,16 +1,75 @@
-import React from 'react'
+import {useState} from 'react'
+import axios from "axios"
 
 const AgregarCobro = () => {
+    const [id_deudor, setId_deudor] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [conceptos, setConceptos] = useState([]);
+    const [showConceptos, setShowConceptos] = useState(false);
+    let conceptosArr = [];
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      const data = {
+        id_deudor,
+        telefono,
+        conceptos: conceptosArr
+      };
+  
+      console.log(data)
+   
+      try {
+        const url = "http://localhost:3000/cobro/crear";
+        const response = await axios.post(url, data);
+        console.log("response", response)
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+  
+    const getConceptos = async (input) => {
+      setConceptos[[]];
+      conceptosArr = []
+      try {
+        const url = `http://localhost:3000/cobro/buscar?folio=${input}`;
+        const { data } = await axios.get(url);
+        setConceptos(data);
+        setShowConceptos(true);
+        console.log(conceptos.length);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    const hadleCategory = (input) => {
+      setConceptos(input);
+      getConceptos(input);
+    };
+    
+    const handleSelectConceptos = (id) => {
+      const concepto = conceptos.find(p => p.id === id);
+      
+      if (concepto) {
+          conceptosArr.push(concepto.id);
+          console.log(conceptosArr)
+      }
+  }
+
   return (
     <div>
       <div class="wrapper">
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <h1>Registro de Cobros</h1>
           <div class="input-box">
-            <input type="text" placeholder="ID" required />
+            <input onChange={(event) => setId_deudor(event.target.value)} type="text" placeholder="ID_Deudor" required />
+          </div>
+          <div class="input-box">
+            <input onChange={(event) => setTelefono(event.target.value)} type="text" placeholder="Telefono" required />
           </div>
           <div>
-            <select name="Conceptos" id="Conceptos" class="box">
+            <select name="Conceptos" id="Conceptos" class="box"
+            onChange={(event) => hadleCategory(event.target.value)}>
               <option value="administrativo" class="vl">
                 Mensualidad
               </option>
@@ -19,7 +78,7 @@ const AgregarCobro = () => {
               </option>
             </select>
           </div>
-          <button type="submit" class="btn">
+          <button type="submit" class="btn" onClick={() => handleSelectConceptos(conceptos.id)} >
             Registrar
           </button>
         </form>
